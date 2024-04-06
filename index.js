f89ecc0f83b310ebac310def2b156869ba54a6ea
Aman <mramanswami@gmail.com>
2024-04-06 18:45:35 +0530

    add cors origin and credentials

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 
 const port = process.env.PORT || 5000;
 const app = express();
-app.use(cors());
+app.use(cors({origin:"*", credentials:true}));
 connectToDB();
 app.use(express.json());
 
