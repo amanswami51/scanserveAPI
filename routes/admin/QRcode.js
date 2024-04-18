5ff05f89d5da645655302414f7719fa0b98f0c42
Aman <mramanswami@gmail.com>
2024-04-18 16:55:59 +0530

    Many qr code is generated

--- routes/admin/QRcode.js
+++ routes/admin/QRcode.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const qrcode = require('qrcode');
+const Fetchuser = require('../../middleware/Fetchuser');
 
 
 // '/api/qrcode' route
-router.get('/', (req, res)=>{
-    const url = 'https://scanserve-88.web.app/user/menufirstpage';
-    qrcode.toDataURL(url, (err, qrCodeUrl)=>{
-        if(err){
-            return res.status(500).json({success:false, err});
-        }else{
-            return res.json({success:true, qrCodeUrl});
-        }
-    })
+router.get('/', Fetchuser, async(req, res)=>{
+
+    const url = req.header('url');   // const url = 'https://scanserve-88.web.app/user/menufirstpage';
+    const totalqrcode = req.header('totalqrcode');
+    const userId = user.id;
+
+    try{
+        var qrCodeArr = []; 
+        for(let table=1; table<=totalqrcode; table++){
+            const websiteLink = `${url}/:${table}/:${userId}`;
+            const x = await qrcode.toDataURL(websiteLink)
+            qrCodeArr.push(x);
+        } 
+        return res.status(200).json({success:true, qrCodeArr});
+
+    }
+    catch(error){
+        return res.status(500).json({success:false, error});
+    }
 })
+
 
 module.exports = router;
